55cb584b21358432ea10b7b2e1f7c1d5b50df14c
ExploitHub <33017598+0exploithub@users.noreply.github.com>
2017-10-23 12:24:13 +0530

    Add files via upload

---
+++ package.sh
@@ -0,0 +1,34 @@
+#/bin/bash
+apt update && apt upgrade -y
+apt install php -y
+apt install perl -y
+apt install nmap -y
+apt install bash -y
+apt install clang -y
+apt install jq -y
+apt install macchanger -y
+apt install python -y 
+apt install python2 -y
+apt install ruby -y
+apt install git -y
+apt install php -y
+apt install perl -y
+apt install nmap -y
+apt install bash -y
+apt install clang -y
+apt install jq -y
+apt install macchanger -y
+apt install nano -y
+apt install curl -y
+apt install tar -y
+apt install zip -y
+apt install unzip -y
+apt install tor -y
+apt install wcalc -y
+apt install openssl
+apt install bmon -y
+apt install sudo -y
+apt install wget -y
+apt install wcalc -y
+apt install openssl
+apt install bmon -y
